e6478c8765a39f04b97240467d94e62eceadbaa9
Anastasiiasyvak <asyvak@kse.org.ua>
2023-10-24 17:41:21 +0300

    FileReader

--- main.cpp
+++ main.cpp
@@ -1,102 +1,73 @@
 #include <iostream>
-#include <cstring>
+#include <fstream>
+#include <string>
+#include <stdexcept>
+
 using namespace std;
 
-class CaesarCipher {
-private:
-    char* encryptedText;
-
+class IReader{ // загальний інтерфейс для об'єктів ( не надає конкретної реалізації)
 public:
-    CaesarCipher() : encryptedText(nullptr) {}
+    virtual ~IReader(){}
+    virtual string read(const string& path) = 0;
+};
 
-    ~CaesarCipher() {
-        delete[] encryptedText;
-    }
-
-    char* encrypt(const char* rawText, int key) {
-        int length = strlen(rawText);
-        encryptedText = new char[length + 1];
-
-        for (int i = 0; i < length; i++) {
-            char currentChar = rawText[i];
-            if (isupper(currentChar)) {
-                currentChar = char(int(currentChar + key - 65) % 26 + 65);
-            } else if (islower(currentChar)) {
-                currentChar = char(int(currentChar + key - 97) % 26 + 97);
-            }
-            encryptedText[i] = currentChar;
+class FileReader : public IReader{
+public:
+    virtual string read(const string& path) override {
+        ifstream file(path);
+        if (!file) {
+            throw runtime_error("File not found");
         }
 
-        encryptedText[length] = '\0';
-
-        return encryptedText;
-    }
-
-    char* decrypt(int key1) {
-        int length = strlen(encryptedText);
-        char* result = new char[length + 1];
-
-        key1 = key1 % 26;
-
-        for (int i = 0; i < length; i++) {
-            char currentChar = encryptedText[i];
-            if (isupper(currentChar)) {
-                currentChar = char(int(currentChar - key1 - 65 + 26) % 26 + 65);
-            } else if (islower(currentChar)) {
-                currentChar = char(int(currentChar - key1 - 97 + 26) % 26 + 97);
-            }
-            result[i] = currentChar;
+        string content;
+        string line;
+        while (getline(file, line)){
+            content += line + '\n';
         }
-
-        result[length] = '\0';
-
-        return result;
-    }
-
-    const char* getEncryptedText() const {
-        return encryptedText;
+        return content;
     }
 };
 
+
 int main() {
-    char rawText[100];
-    int key = 0;
-    int key1 = 0;
-    CaesarCipher cipher;
+    string filePath; // /home/nastia/CLionProjects/forth_assignment/file.txt
+    cout << "Enter the path to the file: ";
+    cin >> filePath;
 
-    while (true) {
-        int command = 0;
-        std::cout << "\nChoose the command: \n1 encrypt command \n2 - decrypt command \n";
-        std::cin >> command;
-        std::cin.ignore();
-
-        switch (command) {
-            case 1:
-                cout << "Enter the rawText:\n";
-                cin.getline(rawText, sizeof(rawText));
-                cout << "Enter our key:\n";
-                cin >> key;
-                cout << "Text : " << rawText;
-                cout << "\nKey: " << key;
-                cipher.encrypt(rawText, key);
-                cout << "\nEncrypted text: " << cipher.getEncryptedText();
-                break;
-
-            case 2:
-                if (cipher.getEncryptedText()) {
-                    cout << "Enter key for decrypting:\n";
-                    cin >> key1;
-                    char* decryptedText = cipher.decrypt(key1);
-                    cout << "\nDecrypted text:  " << decryptedText;
-                    delete[] decryptedText;
-                } else {
-                    cout << "\nNo encrypted text to decrypt.";
-                }
-                break;
-
-            default:
-                cout << "\nInvalid command.";
-                break;
-        }
+    IReader* reader = new FileReader();
+    try {
+        string fileContent = reader->read(filePath);
+        cout << "File content : \n " << fileContent;
     }
+    catch (const exception& exception) {
+        cerr << "Error: " << exception.what() << endl;
+    }
+
+    delete reader;
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
