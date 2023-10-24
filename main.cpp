3142c16c04a171e034d5d3530db726c7e94e9d4d
Anastasiiasyvak <asyvak@kse.org.ua>
2023-10-24 18:30:36 +0300

    Iwriter

--- main.cpp
+++ main.cpp
@@ -5,13 +5,19 @@
 
 using namespace std;
 
-class IReader{ // загальний інтерфейс для об'єктів ( не надає конкретної реалізації)
+class IReader {
 public:
-    virtual ~IReader(){}
+    virtual ~IReader() {}
     virtual string read(const string& path) = 0;
 };
 
-class FileReader : public IReader{
+class IWriter {
+public:
+    virtual ~IWriter() {}
+    virtual void write(const string& path, const string& content) = 0;
+};
+
+class FileReader : public IReader {
 public:
     virtual string read(const string& path) override {
         ifstream file(path);
@@ -21,53 +27,65 @@ public:
 
         string content;
         string line;
-        while (getline(file, line)){
+        while (getline(file, line)) {
             content += line + '\n';
         }
         return content;
     }
 };
 
+class FileWriter : public IWriter {
+public:
+    virtual void write(const string& path, const string& content) override {
+        ifstream file(path);
+        if (file) {
+            file.close();
+            throw runtime_error("File already exists");
+        }
+
+        ofstream outputFile(path);
+        if (!outputFile) {
+            throw runtime_error("Error opening the file for writing");
+        }
+        outputFile << content;
+    }
+};
+
 
 int main() {
-    string filePath; // /home/nastia/CLionProjects/forth_assignment/file.txt
-    cout << "Enter the path to the file: ";
-    cin >> filePath;
+    string filePath;
+    int command;
 
-    IReader* reader = new FileReader();
-    try {
-        string fileContent = reader->read(filePath);
-        cout << "File content : \n " << fileContent;
-    }
-    catch (const exception& exception) {
-        cerr << "Error: " << exception.what() << endl;
+    cout << "Choose the command:\n";
+    cout << "1 - Read file\n";
+    cout << "2 - Write to file\n";
+    cin >> command;
+
+    if (command == 1) {
+        cout << "Enter the path to the file: ";
+        cin >> filePath;
+        IReader* reader = new FileReader();
+        try {
+            string fileContent = reader->read(filePath);
+            cout << "File content:\n" << fileContent;
+        } catch (const exception& exception) {
+            cerr << "Error: " << exception.what() << endl;
+        }
+    } else if (command == 2) {
+        cout << "Enter the path to the file: ";
+        cin >> filePath;
+        IWriter* writer = new FileWriter();
+        try {
+            cout << "Enter new content to write to the file: ";
+            string newContent;
+            cin.ignore();
+            getline(cin, newContent);
+            writer->write(filePath, newContent);
+            cout << "File has been updated with new content." << endl;
+        } catch (const exception& exception) {
+            cerr << "Error: " << exception.what() << endl;
+        }
     }
 
-    delete reader;
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
